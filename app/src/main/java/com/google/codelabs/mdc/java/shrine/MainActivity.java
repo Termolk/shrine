38ee3225a6f63f74cba60c383d191a538e5c1275
Сергей Логачев <sergejlogacev@MacBook-Pro-Sergej.local>
2021-12-26 09:03:14 +0300

    Initial commit

---
+++ app/src/main/java/com/google/codelabs/mdc/java/shrine/MainActivity.java
@@ -0,0 +1,40 @@
+package com.google.codelabs.mdc.java.shrine;
+
+import android.os.Bundle;
+import android.text.Editable;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity implements NavigationHost {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.shr_main_activity);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.container, new LoginFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public void navigateTo(Fragment fragment, boolean addToBackstack) {
+        FragmentTransaction transaction =
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.container, fragment);
+
+        if (addToBackstack) {
+            transaction.addToBackStack(null);
+        }
+
+        transaction.commit();
+    }
+}
